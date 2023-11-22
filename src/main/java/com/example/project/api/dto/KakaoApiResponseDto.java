3ccb5f46532fd1e75f87293ba5e419fb02ebdc0a
Dylan5023 <kangjun5023@gmail.com>
2023-11-21 16:34:55 -0800

    Implementing a docker multi-container environment

---
+++ src/main/java/com/example/project/api/dto/KakaoApiResponseDto.java
@@ -0,0 +1,4 @@
+package com.example.project.api.dto;
+
+public class KakaoApiResponseDto {
+}
---
+++ src/main/java/com/example/project/api/dto/MetaDto.java
@@ -0,0 +1,9 @@
+package com.example.project.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class MetaDto {
+
+    @JsonProperty("total_count")
+    private Integer totalCount;
+}
