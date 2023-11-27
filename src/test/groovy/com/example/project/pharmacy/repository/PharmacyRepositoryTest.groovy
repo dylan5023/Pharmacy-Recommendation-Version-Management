36482f7ce1da27a6d1c0abf1e5614832fa2ae9a5
Dylan5023 <kangjun5023@gmail.com>
2023-11-26 16:06:02 -0800

    Automated creation/modification time with JPA Auditing

--- src/test/groovy/com/example/project/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/example/project/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -4,6 +4,8 @@ import com.example.project.AbstractIntegrationContainerBaseTest
 import com.example.project.pharmacy.entity.Pharmacy
 import org.springframework.beans.factory.annotation.Autowired
 
+import java.time.LocalDateTime
+
 
 class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest{
 
@@ -58,5 +60,25 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest{
 
         then:
         result.size() == 1
+    }
+
+    def "Register BaseTimeEntity"() {
+        given:
+        LocalDateTime now = LocalDateTime.now()
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .build()
+        when:
+        pharmacyRepository.save(pharmacy)
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.get(0).getCreatedDate().isAfter(now)
+        result.get(0).getModifiedDate().isAfter(now)
+
     }
 }
